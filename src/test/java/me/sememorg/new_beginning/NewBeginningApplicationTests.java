5899e66bb66a96422c433f7ad2f89b7d89848c0d
Londers <semen-morgyynenko@mail.ru>
2019-08-20 11:58:23 +0600

    MVC beginning

--- src/test/java/me/sememorg/new_beginning/NewBeginningApplicationTests.java
+++ src/test/java/me/sememorg/new_beginning/NewBeginningApplicationTests.java
@@ -2,15 +2,24 @@ package me.sememorg.new_beginning;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NewBeginningApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:8080/greeting", String.class)).contains("Hello World");
 	}
 
 }
